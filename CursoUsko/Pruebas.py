e7b3fac913912f9b1089775efb3c9d1e9b63d34d
JCA <3029392960107@ingenieria.usac.edu.gt>
2021-08-17 00:48:27 -0600

    agregando para git remoto

--- CursoUsko/Pruebas.py
+++ CursoUsko/Pruebas.py
@@ -1,26 +1,5 @@
-def generarMultipolosDe7(limite):
-    numero = 1
-    listaNumeros = []
+miDiccionario = {"España":11, "Peru":10,"Alemania":7}
 
-    while numero <= limite:
-        listaNumeros.append(numero*7)
-        numero +=1
-    return listaNumeros
+miDiccionario["Guatemala"]= 15
 
-
-def generadorMultiplosDe7(limite):
-    numero = 1
-    while numero <= limite:
-        yield numero * 7 
-        numero +=1
-
-obtenerMultiplosDe7 = generadorMultiplosDe7(5)
-
-for n in obtenerMultiplosDe7:
-    print(n)
-
-print(next(obtenerMultiplosDe7))
-print("aca hay 1000 lineas de codigo")
-print(next(obtenerMultiplosDe7))
-print("aca hay muchas lineas de codigo")
-print(next(obtenerMultiplosDe7))
+print(miDiccionario)
