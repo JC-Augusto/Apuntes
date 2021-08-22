9cf2b35d22b54fe79cea55c3d79f326d8e4466b1
JCA <3029392960107@ingenieria.usac.edu.gt>
2021-08-22 16:05:46 -0600

    Uso de clases agregada

--- CursoUsko/Pruebas.py
+++ CursoUsko/Pruebas.py
@@ -1,5 +1,16 @@
-miDiccionario = {"España":11, "Peru":10,"Alemania":7}
+class Curso(): 
 
-miDiccionario["Guatemala"]= 15
+    def __init__(self,nombre,creditos, profesion):
+        self.nombre = nombre
+        self.creditos = creditos
+        self.curso = profesion
+        self.__imparticion = "Presencial" #Propiedad encapsulada
 
-print(miDiccionario)
+    def mostrarDatos(self):
+        dat = "Nombre: {0} / Creditos: {1} / Modo de impartición: {2}"
+        print(dat.format(self.nombre, self.creditos, self.__imparticion))
+
+curso1 = Curso("Matemática",5,"Ingenieria")
+curso1.mostrarDatos()
+print(curso1.nombre)
+print(curso1.__imparticion)
